59e07e011587b0792cafde610cf1caef9a12ac82
FastWorldDalex <geampier.smc@gmail.com>
2023-01-17 23:15:38 -0500

    CAMBIOS

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './pages/home/home.component';
 import { SharedModule } from '../../shared/shared.module';
-import { CalendarComponent } from './pages/home/calendar/calendar.component';
+import { HomeComponent } from './pages/home.component';
+import { CalendarComponent } from './pages/components/calendar/calendar.component';
 
 
 
