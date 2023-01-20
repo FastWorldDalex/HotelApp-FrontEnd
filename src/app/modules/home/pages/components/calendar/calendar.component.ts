c7502f948f8a9d41122a4b5ab29d8917b1af0699
Juan Carlos Rodriguez Yarmas <jcry87@gmail.com>
2023-01-20 11:43:03 -0500

    Improves in Calendar Configuration

--- src/app/modules/home/pages/components/calendar/calendar.component.ts
+++ src/app/modules/home/pages/components/calendar/calendar.component.ts
@@ -23,13 +23,14 @@ export class CalendarComponent implements OnInit {
   constructor(private nodeService: NodeService) {}
 
   ngOnInit() {
-      this.nodeService.getEvents().then((events) => {
+      /*this.nodeService.getEvents().then((events) => {
           this.events = events;
           this.options = { ...this.options, ...{ events: events } };
-      });
+      });*/
 
       this.options = {
           plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+          initialView: 'timeGridWeek', // dayGridWeek
           initialDate: '2023-01-18',
           locale:esLocale,
           headerToolbar: {
@@ -37,6 +38,41 @@ export class CalendarComponent implements OnInit {
               center: 'title',
               right: ''
           },
+          eventSources: [
+
+            // your event source
+              {
+                events: [ // put the array in the `events` property
+                  {
+                    title  : '<span>Carla Marin</span> <br>+51966710491 <br> carlamarin@gmail.com <br> 1 adulto - 0 niños',
+                    start  : '2023-01-18T00:00:00',
+                    end  : '2023-01-18T01:00:00',
+                  },
+                  {
+                    title  : '<span>Carmen Villaverde</span> <br>  +51966710491 <br> carmenvillaverde@gmail.com <br> 2 adultos - 0 niños',
+                    start  : '2023-01-20T02:00:00',
+                    end  : '2023-01-20T03:00:00',
+                  },
+                  {
+                    title  : '<span>Pedro Rivas</span> <br>  +51966744497 <br> pedrorivas@gmail.com <br> 1 adulto - 1 niño',
+                    start  : '2023-01-21T01:00:00',
+                    end  : '2023-01-21T02:00:00',
+                  }
+                ],
+
+                backgroundColor: '#2962FF',
+                //color: 'blue',     // an option!
+                textColor: 'white' // an option!
+              }
+            ],
+            eventContent: function(info ) {
+              return {html: '<div class="event-content">' + info.event.title + '</div>'};
+            },
+          //eventShortHeight: 60,
+          //slotEventOverlap: true,
+          slotDuration: '00:15:00',
+          nowIndicator: true,
+          allDaySlot: false,
           editable: true,
           selectable: true,
           selectMirror: true,
