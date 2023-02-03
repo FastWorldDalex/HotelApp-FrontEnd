6f9bafbfe2a4633ba2beb3e9b86360eb52b4a1b1
Juan Carlos Rodriguez Yarmas <jcry87@gmail.com>
2023-02-02 23:12:31 -0500

    Improves in admin v2

--- src/app/modules/admin/pages/admin/component/main-content/main-content.component.ts
+++ src/app/modules/admin/pages/admin/component/main-content/main-content.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-main-content',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./main-content.component.scss']
 })
 export class MainContentComponent {
-
+  collapsed: boolean = true;
 }
