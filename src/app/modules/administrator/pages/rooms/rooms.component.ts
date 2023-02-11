8cfffac2652f8e8d04a8ce04a8d581feafcf45d7
Juan Carlos Rodriguez Yarmas <jcry87@gmail.com>
2023-02-10 19:27:44 -0500

    Improves in rooms table

--- src/app/modules/administrator/pages/rooms/rooms.component.ts
+++ src/app/modules/administrator/pages/rooms/rooms.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Titles } from 'src/app/shared/interface/interfaces';
+import { Room } from './interface/iroom';
 
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.component.html',
   styleUrls: ['./rooms.component.scss']
 })
-export class RoomsComponent {
 
+export class RoomsComponent implements OnInit{
+  titulos: Titles[] = [];
+  ltsRooms: Room[] = [];
+  room: Room;
+
+  constructor(
+  ) {
+    this.room = new Room();
+  }
+
+  ngOnInit() {
+
+  }
+
+  componentsInitials(){
+    this.titulos = [
+      {title: '#', width: 2},
+      {title: 'Nombres', width: 2},
+      {title: 'Apellidos', width: 2},
+      {title: 'Documento', width: 2},
+      {title: 'Teléfono', width: 2},
+      {title: 'Email', width: 2},
+      {title: 'País', width: 2},
+      {title: 'Cantidad de Reservas', width: 2},
+      {title: 'Última Reserva', width: 2},
+      {title: 'Fecha de Creación', width: 2},
+      {title: 'Estado', width: 2},
+      {title: 'Acciones', width: 2}
+    ];
+  }
 }
